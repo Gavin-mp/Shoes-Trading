346103b5c5f3cae6c42ca8080b1678d1cdd29cd5
BianZengXu <1150111387@qq.com>
2020-11-19 12:27:02 +0800

    边增旭

---
+++ sale_shoes/src/main/java/com/niit/pojo/Shoes.java
@@ -0,0 +1,131 @@
+package com.niit.pojo;
+
+public class Shoes {
+	private int shoesid;
+	private String shoesname;
+	private String shoesclass;
+	private String s40;
+	private String s41;
+	private String s42;
+	private String s43;
+	private String s44;
+	private String s45;
+	private String s46;
+	private String shoesimg;
+	private String shoesprice;
+	
+	public Shoes() {
+	}
+
+	public Shoes(int shoesid, String shoesname, String shoesclass, String s40, String s41, String s42, String s43,
+			String s44, String s45, String s46, String shoesimg, String shoesprice) {
+		this.shoesid = shoesid;
+		this.shoesname = shoesname;
+		this.shoesclass = shoesclass;
+		this.s40 = s40;
+		this.s41 = s41;
+		this.s42 = s42;
+		this.s43 = s43;
+		this.s44 = s44;
+		this.s45 = s45;
+		this.s46 = s46;
+		this.shoesimg = shoesimg;
+		this.shoesprice = shoesprice;
+	}
+
+	public int getShoesid() {
+		return shoesid;
+	}
+
+	public void setShoesid(int shoesid) {
+		this.shoesid = shoesid;
+	}
+
+	public String getShoesname() {
+		return shoesname;
+	}
+
+	public void setShoesname(String shoesname) {
+		this.shoesname = shoesname;
+	}
+
+	public String getShoesclass() {
+		return shoesclass;
+	}
+
+	public void setShoesclass(String shoesclass) {
+		this.shoesclass = shoesclass;
+	}
+
+	public String getS40() {
+		return s40;
+	}
+
+	public void setS40(String s40) {
+		this.s40 = s40;
+	}
+
+	public String getS41() {
+		return s41;
+	}
+
+	public void setS41(String s41) {
+		this.s41 = s41;
+	}
+
+	public String getS42() {
+		return s42;
+	}
+
+	public void setS42(String s42) {
+		this.s42 = s42;
+	}
+
+	public String getS43() {
+		return s43;
+	}
+
+	public void setS43(String s43) {
+		this.s43 = s43;
+	}
+
+	public String getS44() {
+		return s44;
+	}
+
+	public void setS44(String s44) {
+		this.s44 = s44;
+	}
+
+	public String getS45() {
+		return s45;
+	}
+
+	public void setS45(String s45) {
+		this.s45 = s45;
+	}
+
+	public String getS46() {
+		return s46;
+	}
+
+	public void setS46(String s46) {
+		this.s46 = s46;
+	}
+
+	public String getShoesimg() {
+		return shoesimg;
+	}
+
+	public void setShoesimg(String shoesimg) {
+		this.shoesimg = shoesimg;
+	}
+
+	public String getShoesprice() {
+		return shoesprice;
+	}
+
+	public void setShoesprice(String shoesprice) {
+		this.shoesprice = shoesprice;
+	}
+}
